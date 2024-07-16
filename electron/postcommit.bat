ac16a44a7d4e9e2bcd5555df9d1f17d2ada77351
Jeff Borden <jeff@kindbbuds.us>
2024-07-15 21:27:50 -0600

    chore(release): v~1,-1

--- electron/postcommit.bat
+++ electron/postcommit.bat
@@ -7,6 +7,12 @@ for /f "tokens=3 delims=:, " %%i in ('findstr /ri "\"version\"" package.json') d
 )
 set "VERSION=%VERSION:~1,-1%"
 
+rem Delete the local tag if it exists
+git tag -d v%VERSION%
+
+rem Delete the remote tag if it exists
+git push origin :refs/tags/v%VERSION%
+
 rem Commit, tag, and push
 git commit -m "chore(release): v%VERSION%" && git tag v%VERSION% && git push origin main && git push origin main --tags
 

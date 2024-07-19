038932a408266712273896fb559146de466dce61
Jeff Borden <jeff@kindbbuds.us>
2024-07-18 20:50:59 -0600

    chore(release): v~1,-1

--- electron/pre-release.js
+++ electron/pre-release.js
@@ -1,6 +1,7 @@
 const { execSync } = require('child_process');
 const { version } = require('./package.json');
 
+
 try {
     // Check for uncommitted changes
     execSync('git diff-index --quiet HEAD');
@@ -9,11 +10,17 @@ try {
     try {
         // Stage all changes
         execSync('git add .');
-        // Commit changes with the current version in the message
-        execSync(`git commit -m "Releasing v${version}"`);
-        // Push changes to the remote repository
-        execSync('git push origin main --follow-tags');
-        console.log(`Changes committed and pushed with message: "Releasing v${version}"`);
+        // Check if there are staged changes
+        const stagedChanges = execSync('git diff --cached --name-only').toString().trim();
+        if (stagedChanges) {
+            // Commit changes with the current version in the message
+            execSync(`git commit -m "Releasing v${version}"`);
+            // Push changes to the remote repository
+            execSync('git push origin main --follow-tags');
+            console.log(`Changes committed and pushed with message: "Releasing v${version}"`);
+        } else {
+            console.log('No changes to commit.');
+        }
     } catch (err) {
         console.error('Failed to commit and push changes:', err.message);
         process.exit(1);
